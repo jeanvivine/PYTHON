560a2a55f7c3ce72c2df3aea6a1d66afe1ee7b82
jeanvivine <jean.vivine@gmail.com>
2022-01-21 22:34:20 +0100

    English

--- PC/Process_checkerV1.py
+++ PC/Process_checkerV1.py
@@ -9,7 +9,7 @@ import sys
 import psutil
 import subprocess
 
-lconnectionreseau = []
+lnetworkconnection = []
 dWhiteList = {}
 dGrelist_to_try = {}
 dDifference_pid = {}
@@ -33,19 +33,19 @@ def menu():
                                         '------| ----- |
                                          JV    '-------'
              ***************************************************************
-             Assurez-vous d'avoir positionne le fichier Creation_list.sh 
-                               dans le meme repertoire !""")
-
+                                    Creation_list.sh
+                            have to be in the same folder !""")
+    print()
     print()
     choice = input("""
-                      A: Creation de la liste BLANCHE
-                      Z: Creation de la liste GRISE
-                      E: Verification de Process/Hash malveillant
-                      R: Verification des Process malveillant avec Reseau
-                      T: Magic
-                      Y: Quitter
+                      A: Creation of WHITE list
+                      Z: Creation of GREY list
+                      E: Verification of evil Process/Hash 
+                      R: Verification of evil Process using network
+                      T: MAGIC
+                      Y: Leave
 
-                      Merci d'entrer un choix: """)
+                      Please make a choice: """)
 
     if choice.lower() == "a":
         creation_liste_blanche()
@@ -60,23 +60,22 @@ def menu():
     elif choice.lower() == "y":
         sys.exit
     else:
-        print("Vous devez choisir entre A, Z, E, R, T ou Y !")
+        print("You have to choose beetween A, Z, E, R, T ou Y !")
         print("Try again !")
-        menu()  # ou main ?
-# A voir si cela est pertinent La regenerer a chaque appel semble pertinenet
+        menu()  # or main ?
 
 
 def creation_liste_blanche():
     # Verifier si le fichier existe ou non
     if os.path.isfile('Process_Hash.txt'):
         print()
-        print("La liste blanche est deja presente !")
+        print("WHITE list is already present !")
         print()
         menu()
     else:
         subprocess.call(['./Creation_list.sh'])
         print()
-        print("Creation de la liste BLANCHE  -> OK !")
+        print("Creation of WHITE list  -> OK !")
         print()
         print()
         menu()
@@ -86,13 +85,13 @@ def creation_liste_grise():
     # Verifier si le fichier existe ou non
     if os.path.isfile('Process_Hash_grey.txt'):
         print()
-        print("La liste grise est deja presente !")
+        print("GREY list is already present !")
         print()
         menu()
     else:
         subprocess.call(['./Creation_list.sh'])
         print()
-        print("Creation de la liste GRISE  -> OK !")
+        print("Creation of GREY list  -> OK !")
         print()
         print()
         menu()
@@ -102,33 +101,33 @@ def export_resultat():
     global dGrelist_to_try
     global dWhiteList
     global dDifference_pid
-    global lconnectionreseau
+    global lnetworkconnection
 
     print()
-    print("Export en cours ...")
-    print("Export vers Investigation.csv  -> OK !")
+    print("Export in progress ...")
+    print("Exporting to Investigation.csv  -> OK !")
 
     with open('Investigation.csv', 'a') as Investigation:
         for process, hash in dGrelist_to_try.items():
             if process not in dWhiteList.keys():
-                print("PROCESS suspect detecte ! ! ! ", process,
-                "--- HASH du process suspect:", hash, "DATE:",
+                print("Suspect PROCESS  found ! ! ! ", process,
+                "--- HASH of suspect PROCESS:", hash, "DATE:",
                 datetime.today(), file=Investigation)
 
     with open('Investigation.csv', 'a') as Investigation:
         for process, hash in dGrelist_to_try.items():
             if hash not in dWhiteList.values():
-                print("HASH suspect detecte ! ! ! ", hash,
-                "--- PROCESS suspect:", process, "DATE:",
+                print("Suspect HASH  found ! ! ! ", hash,
+                "--- PROCESS of suspect HASH:", process, "DATE:",
                 datetime.today(), file=Investigation)
 
     with open('Investigation.csv', 'a') as Investigation:
-        for pid in lconnectionreseau:
+        for pid in lnetworkconnection:
             if str(pid.pid) in dDifference_pid.keys():
-                print("Le Process", dDifference_pid[str(pid.pid)], "pid:", pid.pid,
-                      " utilise le reseau", pid.laddr, pid.raddr, pid.status,
-                      pid.type, "DATE:", datetime.today(),
-                      file=Investigation)
+                print("The Process", dDifference_pid[str(pid.pid)], "pid:",
+                    pid.pid, " is using Network", pid.laddr, pid.raddr,
+                    pid.status, pid.type, "LINK:", datetime.today(),
+                    file=Investigation)
 
     menu()
 
@@ -156,21 +155,20 @@ def verification_malveillant():
 
         for process, hash in dGrelist_to_try.items():
             if process not in dWhiteList.keys():
-                print("PROCESS suspect detecte ! ! ! ", process, "--- HASH \
-du process suspect:", hash, "DATE:", datetime.today())
+                print("Suspect PROCESS  found ! ! ! ", process, "--- HASH \
+                    of suspect PROCESS :", hash, "DATE:", datetime.today())
 
         print()
         print()
 
         for process, hash in dGrelist_to_try.items():
             if hash not in dWhiteList.values():
-                print("HASH suspect detecte ! ! ! ", hash, "--- PROCESS \
-suspect:", process, "DATE:", datetime.today())     
+                print("Suspect HASH found ! ! ! ", hash, "--- PROCESS \
+                suspect:", process, "DATE:", datetime.today())     
         menu()
     else:
         print()
-        print("Merci de proceder a la creation de la creation de la liste \
-GRISE au prealable !")
+        print("Create GREY list first ! ! !")
         print()
         print()
         menu()
@@ -179,7 +177,7 @@ GRISE au prealable !")
 def check_reseau():
     dGrelist_to_try_pid = {}
     dWhitelist_to_try_pid = {}
-    global lconnectionreseau
+    global lnetworkconnection
     global dDifference_pid
 #    #Generation dico avec le PID
     vFiletoparse_grey_pid = open("Process_Hash_grey.txt")
@@ -196,12 +194,12 @@ def check_reseau():
         if process not in dWhitelist_to_try_pid.values():
             dDifference_pid.update({pid: process})
 
-    lconnectionreseau = psutil.net_connections()
-    for pid in lconnectionreseau:
+    lnetworkconnection = psutil.net_connections()
+    for pid in lnetworkconnection:
         if str(pid.pid) in dDifference_pid.keys():
-            print("Le Process", dDifference_pid[str(pid.pid)], "pid:",
-pid.pid, " utilise le reseau", pid.laddr[0], pid.laddr[1], pid.raddr,
-pid.type, "LIEN:", pid.status)
+            print("The Process", dDifference_pid[str(pid.pid)], "pid:",
+                pid.pid, " is using Network", pid.laddr[0], pid.laddr[1],
+                pid.raddr, pid.type, "LINK:", pid.status)
     print()
     menu()
 
